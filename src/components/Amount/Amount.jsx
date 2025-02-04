0ada4c1c6dcba436cbc1e99b5e042140df8b6359
Ivan Tihonov <ivan.tixonov000@gmail.com>
2025-02-04 22:52:11 +0700

    Вывод денег

--- src/components/Amount/Amount.jsx
+++ src/components/Amount/Amount.jsx
@@ -1,10 +1,27 @@
 import './amount.css';
+import {useEffect, useState} from "react";
 
 export default function Amount() {
+    const [money, setMoney] = useState(localStorage.getItem('money') || '0');
+
+    useEffect(() => {
+        const handleStorageChange = () => {
+            setMoney(localStorage.getItem('money') || '0');
+        };
+
+        window.addEventListener('storage', handleStorageChange);
+
+        return () => window.removeEventListener('storage', handleStorageChange);
+    }, []);
+
     return (
         <section id="amount">
             <h2>Слушай, я тут подсчитал...</h2>
-            <p>У тебя на счету 1000 руб. Поздравляю тебя!</p>
+            {money !== '0' ? (
+                <p>У тебя на счету {money} руб. Поздравляю тебя!</p>
+            ) : (
+                <p>Ты пока что нищий :(</p>
+            )}
             <p>А сегодня ты заработал 500 руб. Тоже неплохой результат!</p>
         </section>
     )
