2df99f974b7de0ba339f20e9af39bb2119e94f21
Ivan Tihonov <ivan.tixonov000@gmail.com>
2025-02-04 20:56:45 +0700

    Header component

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,20 @@
+import './header.module.css';
+import {useEffect, useState} from "react";
+
+// eslint-disable-next-line react/prop-types
+export default function Header({className = null}) {
+    const [time, setTime] = useState(new Date());
+
+    useEffect(() => {
+        const interval = setInterval(() => setTime(new Date()));
+
+        return () => clearInterval(interval);
+    }, []);
+
+    return (
+        <header className={className}>
+            <h1>Работа</h1>
+            <p>Текущее время: {time.toLocaleTimeString()}</p>
+        </header>
+    )
+}
